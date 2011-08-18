7439bd31b6a0c79c3e4403570b7fe22c588c2cd3
Brad Davis <brad.bdavis1@gmail.com>
2011-08-18 16:00:25 -0400

    lots of little tweeks ;

--- MIP.php
+++ MIP.php
@@ -12,13 +12,20 @@ Version: .01
 Author URI: http://braddavis.cc/
 */
 
+add_action('wp_footer','footer_tipsy');
+
+function footer_tipsy(){
+print "<script>jQuery(document).ready(function(){";
+print "jQuery('#product').tipsy({html:true});});</script>";
+}
+
 function my_callback($matches){
-  $link = 'http://vivid-meadow-496.herokuapp.com/001/products/' . $matches[1];
+  $link = 'http://vivid-meadow-496.herokuapp.com//001/products/' . $matches[1];
    $response = file_get_contents($link);  
    $response = json_decode($response);
    
     $return = '<a id="product"';
-    $return .= 'title="<b>' . $response[0]->description;
+    $return .= 'title="<b>' . $response[0]->desc;
     $return .= '</b><br> in stock: ' . $response[0]->stock . '"';
     $return .= 'href="' . $link . '">';
     $return .= $matches[2];
@@ -41,8 +48,8 @@ add_filter( 'the_content', 'my_the_content_filter' );
 function my_init_method() {
     wp_register_script( 'tipsy', WP_PLUGIN_URL . '/MIP/tipsy/jquery.tipsy.js');
     wp_enqueue_script( 'tipsy' );
-            wp_register_style('myStyleSheets', WP_PLUGIN_URL . '/MIP/tipsy/tipsy.css');
-            wp_enqueue_style( 'myStyleSheets');
+    wp_register_style('myStyleSheets', WP_PLUGIN_URL . '/MIP/tipsy/tipsy.css');
+    wp_enqueue_style( 'myStyleSheets');
 }    
  
 add_action('init', 'my_init_method');
